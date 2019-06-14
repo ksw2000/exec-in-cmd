c7d2ec852efc014516a6ff3998162766cefa6bea
Hadname <50315688+Hadname@users.noreply.github.com>
2019-06-14 13:49:02 +0800

    Add files via upload

--- lib/open.c
+++ lib/open.c
@@ -5,13 +5,13 @@
 #include<unistd.h>
 int execAndGet(char* cmd, char* result){
     char buffer[512];
-    FILE* pipe = _popen(cmd, "r");
-    if (!pipe){
+    FILE* pipe=_popen(cmd,"r");
+    if(!pipe){
         return 0;   //Return 0 if fail.
     }
-    while(!feof(pipe)) {
-    if(fgets(buffer, 128, pipe)){
-            strcat(result,buffer);
+    while(!feof(pipe)){
+        if(fgets(buffer,512,pipe)){
+            sprintf(result,"%s%s",result,buffer);
         }
     }
     _pclose(pipe);
@@ -41,96 +41,54 @@ int main(int argc, char *argv[]){
     int error=0,exit=0,packageFlag=0;
     float compile_time=0.0,exec_time=0.0,total_time=0.0;
     clock_t start_compile_time,end_compile_time;
-    strcat(str,"cd \"");
-    strcat(str,argv[1]);
-    strcat(str,"\"");
+    sprintf(str,"cd \"%s\"",argv[1]);
 	if(strcmp(argv[3],".java")==0){
         //Compile
-		strcat(str," & md out & cls & javac -encoding UTF-8 -d out -classpath out ");
-		strcat(str,argv[2]);
-		strcat(str,argv[3]);
+        sprintf(str,"%s & md out & cls & javac -encoding UTF-8 -d out -classpath out %s%s",str,argv[2],argv[3]);
         compile_time=exec(str);
-        strcpy(str2,str);
+        sprintf(str2,"%s",str);
         clean(str);
 
         //Run
         char getPackage[256]="";
         char packageName[256]="";
+        sprintf(getPackage,"%s\\readPackage.exe -p \"%s\\%s%s\" ",argv[4],argv[1],argv[2],argv[3]);
+        sprintf(str,"cd \"%s\\out\" & java ",argv[1]);
 
-        strcat(getPackage,argv[4]);
-        strcat(getPackage,"\\readPackage.exe -p \"");
-        strcat(getPackage,argv[1]);
-        strcat(getPackage,"\\");
-        strcat(getPackage,argv[2]);
-        strcat(getPackage,argv[3]);
-        strcat(getPackage,"\"");
-
-        strcat(str,"cd \"");
-        strcat(str,argv[1]);
-        strcat(str,"\\out\" & java ");
+        //If use package
         if(execAndGet(getPackage,packageName)==1){
             if(strcmp(packageName,"0")!=0){
-        		strcat(str,packageName);
-        		strcat(str,".");
+                sprintf(str,"%s%s.",str,packageName);
                 packageFlag=1;
             }
-    		strcat(str,argv[2]);
-        }else{
-            printf("file not found");
         }
 
+        sprintf(str,"%s%s",str,argv[2]);
     }else if(strcmp(argv[3],".c")==0 || strcmp(argv[3],".cpp")==0){
-        strcat(str," & chcp 65001 & md out & cls & ");
+        sprintf(str,"%s & chcp 65001 & md out & cls &",str);
 		if(strcmp(argv[3],".c")==0){
-			strcat(str,"gcc");
+            sprintf(str,"%s gcc",str);
 		}else{
-			strcat(str,"g++");
+            sprintf(str,"%s g++",str);
 		}
-        strcat(str," \"");
-        strcat(str,argv[2]);
-        strcat(str,argv[3]);
-		strcat(str,"\" -o \"out\\");
-		strcat(str,argv[2]);
-		strcat(str,"\"");
+        sprintf(str,"%s \"%s%s\" -o \"out\\%s\"",str,argv[2],argv[3],argv[2]);
         compile_time=exec(str);
-        strcpy(str2,str);
+        sprintf(str2,"%s",str);
         clean(str);
-
-        strcat(str,"cd \"");
-        strcat(str,argv[1]);
-        strcat(str,"\\out\" & \"");
-		strcat(str,argv[2]);
-		strcat(str,".exe\"");
+        sprintf(str,"cd \"%s\\out\" & \"%s.exe\"",argv[1],argv[2]);
 	}else if(strcmp(argv[3],".py")==0){
-		strcat(str," & python \"");
-		strcat(str,argv[2]);
-		strcat(str,argv[3]);
-        strcat(str,"\"");
+        sprintf(str,"%s & python \"%s%s\"",str,argv[2],argv[3]);
 	}else if(strcmp(argv[3],".go")==0){
-		strcat(str," & go run \"");
-		strcat(str,argv[2]);
-		strcat(str,argv[3]);
-        strcat(str,"\"");
+        sprintf(str,"%s & go run \"%s%s\"",str,argv[2],argv[3]);
 	}else if(strcmp(argv[3],".R")==0){
-		strcat(str," & chcp 65001 & cls & Rscript \"");
-		strcat(str,argv[2]);
-		strcat(str,argv[3]);
-		strcat(str,"\"");
+        sprintf(str,"%s & chcp 65001 & cls & Rscript \"%s%s\"",str,argv[2],argv[3]);
 	}else if(strcmp(argv[3],".rb")==0){
-		strcat(str," & chcp 65001 & cls & ruby \"");
-		strcat(str,argv[2]);
-		strcat(str,argv[3]);
-		strcat(str,"\"");
+        sprintf(str,"%s & chcp 65001 & cls & ruby \"%s%s\"",str,argv[2],argv[3]);
 	}else if(strcmp(argv[3],".html")==0 || strcmp(argv[3],".htm")==0 || strcmp(argv[3],".pdf")==0 || strcmp(argv[3],".lnk")==0){
-        strcat(str," & start \"\" \"");
-        strcat(str,argv[1]);
-        strcat(str,"\\");
-        strcat(str,argv[2]);
-        strcat(str,argv[3]);
-        strcat(str,"\"");
-        exit=1;     //We don't need command line if we open the types of files
+        sprintf(str,"%s & start \"\" \"%s\\%s%s\"",str,argv[1],argv[2],argv[3]);
+        exit=1; //We don't need cmd window if we open the types of files
     }else{
-        error=(argv[0]==0)? 2:3;
+        error=(argv[0]==0)? 2:1;
     }
 
     if(error==1){
