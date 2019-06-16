0b88ac80bc2f16335cac905bd5d9b654cab25871
Hadname Online <50315688+Hadname@users.noreply.github.com>
2019-06-17 00:59:44 +0800

    Add files via upload

--- lib/open.c
+++ lib/open.c
@@ -18,10 +18,6 @@ int execAndGet(char* cmd, char* result){
     return 1;   //Return 0 if success.
 }
 
-int clean(char *p){
-    *p='\0';
-}
-
 float exec(char str[]){
     clock_t start,end;
     start=clock();
@@ -32,22 +28,22 @@ float exec(char str[]){
 }
 
 int main(int argc, char *argv[]){
-	//strcmp is a function compare two strings. If they are same, return 0
-	//argv[1] return path (not including \)
+    //strcmp is a function compare two strings. If they are same, return 0
+    //argv[1] return path (not including \)
     //argv[2] return filename (not including extension)
     //argv[3] return filename extension (including point)
     //argv[4] return this open.c's path (not including \)
-	char str[1024]="",str2[1024]="";
+    char str[1024]="",str2[1024]="";
     int error=0,exit=0,packageFlag=0;
     float compile_time=0.0,exec_time=0.0,total_time=0.0;
     clock_t start_compile_time,end_compile_time;
     sprintf(str,"cd \"%s\"",argv[1]);
-	if(strcmp(argv[3],".java")==0){
+    if(strcmp(argv[3],".java")==0){
         //Compile
         sprintf(str,"%s & md out & cls & javac -encoding UTF-8 -d out -classpath out %s%s",str,argv[2],argv[3]);
         compile_time=exec(str);
         sprintf(str2,"%s",str);
-        clean(str);
+        str='\0';
 
         //Run
         char getPackage[256]="";
@@ -66,25 +62,25 @@ int main(int argc, char *argv[]){
         sprintf(str,"%s%s",str,argv[2]);
     }else if(strcmp(argv[3],".c")==0 || strcmp(argv[3],".cpp")==0){
         sprintf(str,"%s & chcp 65001 & md out & cls &",str);
-		if(strcmp(argv[3],".c")==0){
+        if(strcmp(argv[3],".c")==0){
             sprintf(str,"%s gcc",str);
-		}else{
+        }else{
             sprintf(str,"%s g++",str);
-		}
+        }
         sprintf(str,"%s \"%s%s\" -o \"out\\%s\"",str,argv[2],argv[3],argv[2]);
         compile_time=exec(str);
         sprintf(str2,"%s",str);
-        clean(str);
+        str='\0';
         sprintf(str,"cd \"%s\\out\" & \"%s.exe\"",argv[1],argv[2]);
-	}else if(strcmp(argv[3],".py")==0){
+    }else if(strcmp(argv[3],".py")==0){
         sprintf(str,"%s & python \"%s%s\"",str,argv[2],argv[3]);
-	}else if(strcmp(argv[3],".go")==0){
+    }else if(strcmp(argv[3],".go")==0){
         sprintf(str,"%s & go run \"%s%s\"",str,argv[2],argv[3]);
-	}else if(strcmp(argv[3],".R")==0){
+    }else if(strcmp(argv[3],".R")==0){
         sprintf(str,"%s & chcp 65001 & cls & Rscript \"%s%s\"",str,argv[2],argv[3]);
-	}else if(strcmp(argv[3],".rb")==0){
+    }else if(strcmp(argv[3],".rb")==0){
         sprintf(str,"%s & chcp 65001 & cls & ruby \"%s%s\"",str,argv[2],argv[3]);
-	}else if(strcmp(argv[3],".html")==0 || strcmp(argv[3],".htm")==0 || strcmp(argv[3],".pdf")==0 || strcmp(argv[3],".lnk")==0){
+    }else if(strcmp(argv[3],".html")==0 || strcmp(argv[3],".htm")==0 || strcmp(argv[3],".pdf")==0 || strcmp(argv[3],".lnk")==0){
         sprintf(str,"%s & start \"\" \"%s\\%s%s\"",str,argv[1],argv[2],argv[3]);
         exit=1; //We don't need cmd window if we open the types of files
     }else{
