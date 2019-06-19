8f9e2e6f672cf0af09c4b99e6d752938a9fe6a69
Hadname Online <50315688+Hadname@users.noreply.github.com>
2019-06-19 09:58:58 +0800

    Add files via upload

--- lib/readPackage.go
+++ lib/readPackage.go
@@ -13,13 +13,13 @@ func main() {
     if err != nil {
         fmt.Print(err)
     }
-
-    re := regexp.MustCompile(`^\s{0,}package\b\s[a-zA-Z]+[a-zA-Z0-9.]{0,};`)
+     
+    re := regexp.MustCompile(`(?m)^\s{0,}package\b\s[a-zA-Z_$]+[a-zA-Z0-9._$]{0,};`)
     first := re.FindStringSubmatch(string(b))
     if len(first)!=0 {
-        re2 := regexp.MustCompile(`[a-zA-Z]+[a-zA-Z0-9.]{0,};`)
+        re2 := regexp.MustCompile(`[a-zA-Z_$]+[a-zA-Z0-9._$]{0,};`)
         second := re2.FindStringSubmatch(first[0]);
-        re3 := regexp.MustCompile(`[a-zA-Z]+[a-zA-Z0-9.]{0,}`)
+        re3 := regexp.MustCompile(`[a-zA-Z_$]+[a-zA-Z0-9._$]{0,}`)
         third := re3.FindStringSubmatch(second[0]);
         fmt.Printf("%s",third[0])
     }else{
