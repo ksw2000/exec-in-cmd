3f57ae14953f80e935313f2f2545a11a373aa0fc
Hadname <admin@had.name>
2020-01-10 14:48:04 +0800

    Delete func:not_support()

--- lib/function.h
+++ lib/function.h
@@ -35,23 +35,3 @@ void error_try_again(){
     system("pause");
     system("cls");
 }
-
-int not_support(char *str){
-    system("chcp 65001 & cls");
-    printf("\033[40;31;1mThe filepath extension is not supported.\033[0m \n");
-    printf("Input \n\t0: Quit\n\t1: Open cmd\n");
-    int choose;
-    scanf("%d",&choose);
-    system("cls");
-    if(choose==0){
-        return 1;
-    }else if(choose==1){
-        sprintf(str,"%s & start",str);
-        system(str);
-        return 1;
-    }else{
-        error_try_again();
-        return not_support(str);
-    }
-    return 0;
-}
