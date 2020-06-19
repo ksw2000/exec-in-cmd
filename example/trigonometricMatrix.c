4f64b25f51c35a6b13fcaabd5e74ec3842e01bb1
liao2000 <liao2000@livemail.tw>
2020-06-19 23:49:48 +0800

    Replace tab to soft tab

--- example/trigonometricMatrix.c
+++ example/trigonometricMatrix.c
@@ -3,26 +3,26 @@
 #include<math.h>
 #define PI 3.14159265
 int main(){
-	int i=0;
-	double rad;
-	printf("%4s","");
-	printf("%7s","sin");
-	printf("%9s","cos");
-	printf("%9s","tan");
-	printf("%9s","cot");
-	printf("%9s","sec");
-	printf("%9s\n","csc");
+    int i = 0;
+    double rad;
+    printf("%4s","");
+    printf("%7s","sin");
+    printf("%9s","cos");
+    printf("%9s","tan");
+    printf("%9s","cot");
+    printf("%9s","sec");
+    printf("%9s\n","csc");
 
-	for(i=0; i<=45; i++){
-		rad=i*PI/180;
-		printf("%2d°  ",i);
-		printf("%7.4lf  ",sin(rad));
-		printf("%7.4lf  ",cos(rad));
-		printf("%7.4lf  ",tan(rad));
-		printf("%7.4lf  ",1/tan(rad));
-		printf("%7.4lf  ",1/cos(rad));
-		printf("%7.4lf  ",1/sin(rad));
-		printf("\n");
-	}
-	return 0;
+    for(i=0; i<=45; i++){
+        rad = i * PI / 180;
+        printf("%2d°  ",i);
+        printf("%7.4lf  ",sin(rad));
+        printf("%7.4lf  ",cos(rad));
+        printf("%7.4lf  ",tan(rad));
+        printf("%7.4lf  ",1/tan(rad));
+        printf("%7.4lf  ",1/cos(rad));
+        printf("%7.4lf  ",1/sin(rad));
+        printf("\n");
+    }
+    return 0;
 }
