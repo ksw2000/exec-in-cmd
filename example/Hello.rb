379cbb703cbb253396169354ec535b2134986f9f
liao2000 <liao2000@livemail.tw>
2020-07-05 14:51:24 +0800

    Update example code

---
+++ example/Hello.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby -w
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+puts "hello world 哈囉 Ruby こんにちは";
--- example/multiplicationTable.go
+++ example/multiplicationTable.go
@@ -1,11 +1,11 @@
 package main
 import "fmt"
 func main(){
-    fmt.Print("Multiplication Table\n")
+    fmt.Println("Multiplication Table")
     for i:=2; i<10; i++{
         for j:=2; j<10; j++{
-            fmt.Printf("%dx%d=%2d\t",j,i,i*j)
+            fmt.Printf("%d×%d = %2d  ", j, i, i*j)
         }
-        fmt.Print("\n")
+        fmt.Println()
     }
 }
